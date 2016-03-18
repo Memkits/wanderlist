1dfd6b5cd1939fef8e1ad388e4002bbbdb5d8390
jiyinyiyong <jiyinyiyong@gmail.com>
2016-03-18 16:17:02 +0800

    generate template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wanderlist
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wanderlist/core_test.clj
@@ -0,0 +1,7 @@
+(ns wanderlist.core-test
+  (:require [clojure.test :refer :all]
+            [wanderlist.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
